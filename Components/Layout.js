cbeb0b5bf2ee3ba8abf618cadbd71afa38858079
Fahmida Sultana Mitu <80353930+FahmidaMitu@users.noreply.github.com>
2023-02-17 21:16:24 +0600

    changes

---
+++ Components/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Layout({ children }) {
+    return ( < >
+            <
+            div >
+            <
+            header > Header < /header> < /
+            div > < / > )
